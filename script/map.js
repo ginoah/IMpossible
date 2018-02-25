ef73ecac7bd6c0b968f158cf9ab5dc31b561a7d8
ginoah <35381270+ginoah@users.noreply.github.com>
2018-02-26 02:14:12 +0800

    add manu

--- script/map.js
+++ script/map.js
@@ -23,13 +23,20 @@ $(".btn").click(function(){
         scrollLeft : document.getElementById("map").scrollLeft + $("#"+id).offset().left
     }, 3000, 'easeInOutExpo');
     $(this).fadeOut(1000);
-    if(id!="home")
+    if(id!="home"){
         setTimeout(function(){$("#btn-home").fadeIn(1000);},3000);
+
+        var lastClass = $('#btn-home').attr('class').split(' ').pop();
+        setTimeout(function(){$("#btn-home").removeClass(lastClass);},3000);
+    }
 });
 
+$(".homebtn").click(function(){
+    $("#manu").fadeOut(1000);
+});
 
 $("#btn-home").click(function(){
-    setTimeout(function(){$(".homebtn").fadeIn(1000);},3000);
+    setTimeout(function(){$(".homebtn").fadeIn(1000); $("#manu").fadeIn(1000);},3000);
 });
 
 $(".informationbtn").click(function(){
@@ -39,7 +46,41 @@ $(".informationbtn").click(function(){
     setTimeout(function(){$("#btn-information-"+idNum).fadeIn(1000);},3000);
 });
 
+$("#btn-information-1").click(function(){
+    $("#btn-home").addClass("rightup");
+});
+
+$("#btn-information-2").click(function(){
+    setTimeout(function(){$("#btn-home").addClass("right");},3000);
+});
+
+$("#btn-information-3").click(function(){
+    setTimeout(function(){$("#btn-home").addClass("rightdown");},3000);
+});
+
+$("#btn-information-4").click(function(){
+    setTimeout(function(){$("#btn-home").addClass("down");},3000);
+});
+
+$("#btn-photos").click(function(){
+    $("#btn-home").addClass("leftdown");
+});
+
 $("#btn-register-1").click(function(){
     setTimeout(function(){$("#btn-register-2").fadeIn(1000);},3000);
-    $("#btn-home").removeClass("down").addClass("left");
+    $("#btn-home").addClass("left");
+});
+
+$("#btn-register-2").click(function(){
+    setTimeout(function(){$("#btn-home").addClass("leftup");},3000);
+});
+
+$("#btn-about").click(function(){
+    $("#btn-home").addClass("up");
+});
+
+$(".listbtn").click(function(){
+    var id = $(this).attr("id");
+    id = id.substring(8);
+    $("#btn-"+id).trigger("click");
 });
