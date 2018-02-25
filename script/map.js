9365a78328ad91267cd7a344264531f4d8b96fd8
ginoah <35381270+ginoah@users.noreply.github.com>
2018-02-26 00:49:21 +0800

    add container div

--- script/map.js
+++ script/map.js
@@ -1,47 +1,45 @@
 
 $(
     function(){
-        $('html, body').animate({
-            scrollTop: $("#a-0").offset().top,
-            scrollLeft: $("#a-0").offset().left
+        $('#map').animate({
+            scrollTop : document.getElementById("map").scrollTop + $("#home").offset().top,
+            scrollLeft : document.getElementById("map").scrollLeft + $("#home").offset().left
         }, 1, 'easeInOutExpo');
-        // $("#black-mask").fadeOut(3000);
     }
 )
 
+var prevpage;
 
 $(".btn").click(function(){
     var audio = new Audio('audio/click.m4a');
     audio.play();
 
-    var id = $(this).attr("id");
-    var idNum = id[id.length-1];
-    $('html, body').animate({
-        scrollTop: $("#a-"+idNum).offset().top,
-        scrollLeft: $("#a-"+idNum).offset().left
-    }, 3000, 'easeInOutExpo');
     $(".btn").fadeOut(1000);
-    if(idNum!="0"){
-        setTimeout(function(){$("#btn-0").fadeIn(1000);},3000);
-        $(".btn").fadeOut(1000);
-    }
-    else{
-        setTimeout(function(){$(".btn").fadeIn(1000);$("#btn-0").fadeOut(0);},3000);
-        $("#btn-0").fadeOut(1000);
-    }
+    var id = $(this).attr("id");
+    id = id.substring(4);
+
+    $("#map").animate({
+        scrollTop : document.getElementById("map").scrollTop + $("#"+id).offset().top,
+        scrollLeft : document.getElementById("map").scrollLeft + $("#"+id).offset().left
+    }, 3000, 'easeInOutExpo');
+    $(this).fadeOut(1000);
+    if(id!="home")
+        setTimeout(function(){$("#btn-home").fadeIn(1000);},3000);
 });
 
 
-$("#btn-0").click(function(){
-    var audio = new Audio('audio/click.m4a');
-    audio.play();
+$("#btn-home").click(function(){
+    setTimeout(function(){$(".homebtn").fadeIn(1000);},3000);
+});
 
+$(".informationbtn").click(function(){
     var id = $(this).attr("id");
     var idNum = id[id.length-1];
-    $('html, body').animate({
-        scrollTop: $("#a-"+idNum).offset().top,
-        scrollLeft: $("#a-"+idNum).offset().left
-    }, 3000, 'easeInOutExpo');
-    $(this).fadeOut(1000);
-    setTimeout(function(){$(".btn").fadeIn(1000);$("#btn-0").fadeOut(0);},3000);
+    idNum++;
+    setTimeout(function(){$("#btn-information-"+idNum).fadeIn(1000);},3000);
+});
+
+$("#btn-register-1").click(function(){
+    setTimeout(function(){$("#btn-register-2").fadeIn(1000);},3000);
+    $("#btn-home").removeClass("down").addClass("left");
 });
